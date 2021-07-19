b602183a8bf75d10cc060fd323a2c024670e698c
Ashifly <aashitapandey20@gmail.com>
2021-07-20 01:58:41 +0530

    Commit

--- sum.java
+++ sum.java
@@ -1,11 +1,9 @@
-public class Sum {
+public class sum {
+    
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
 
-    Run|Debug
-    public static void main(String[] args)
-    {
-      int a = 5;
-      int b = 6;
-
-      System.out.println(a+b);
+        System.out.println("The sum is " + (a+b));
     }
 }
